d15b88c48cfa1db439c3cfa4903f7904d225202e
Sidoine De Wispelaere <sdewispelaere@talentsoft.com>
2016-06-30 09:36:01 +0200

    Import initial

---
+++ ChiffresRomains/ChiffresRomains.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChiffresRomains
+{
+    class ChiffresRomains
+    {
+        static Dictionary<int, string> symbols = new Dictionary<int, string>
+                                              {
+            {  1, "I"},
+            { 5, "V" },
+            { 10, "X" },
+            { 50, "L" },
+            {
+                100, "C"
+            },
+            { 500, "D" },
+            { 1000, "M" }
+        };
+
+        private static Dictionary<char, int> invert; 
+
+        static ChiffresRomains()
+        {
+            invert = symbols.ToDictionary(x => x.Value[0], x => x.Key);
+        }
+
+        public int ConvertToDigits(string roman)
+        {
+            int result = 0;
+            for (var i = 0; i < roman.Length; i++)
+            {
+                int value = invert[roman[i]];
+                if (i < roman.Length - 1)
+                {
+                    int next = invert[roman[i + 1]];
+                    if (next > value) result -= value;
+                    else result += value;
+                }
+                else
+                {
+                    result += value;
+                }
+            } 
+            return result;
+        }
+
+        public string ConvertToRoman(int nombre)
+        {
+            if (nombre <= 0 || nombre > 3000)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var result = new StringBuilder();
+            var orderedSymbols = symbols.OrderByDescending(x => x.Key).ToList();
+            foreach (var symbol in orderedSymbols)
+            {
+                var quotient = nombre / symbol.Key;
+                if (quotient > 3 && CanSubstract(symbol.Key))
+                {
+                    // do stuff
+                    nombre += symbol.Key;
+                    result.Append(symbol.Value);
+                    result.Append(ConvertToRoman(nombre));
+                    return result.ToString();
+                }
+                else
+                {
+                    nombre = nombre % symbol.Key;
+                    for (var i = 0; i < quotient; i++)
+                        result.Append(symbol.Value);
+
+                    if (CanSubstract(symbol.Key) && nombre > 1 && nombre >= 9 * symbol.Key / 10)
+                    {
+                        result.Append(symbols[symbol.Key / 10]);
+                        result.Append(symbols[symbol.Key]);
+                        nombre -= symbol.Key * 9 / 10;
+                    }
+                }
+            }
+            return result.ToString();
+        }
+
+        private bool CanSubstract(int key)
+        {
+            return key == 1 || key == 10 || key == 100 || key == 1000;
+        }
+
+
+    }
+}
---
+++ ChiffresRomains/RomanumChiffrumTesti.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChiffresRomains
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RomanumChiffrumTesti
+    {
+        [TestCase(1, "I")]
+        [TestCase(5, "V")]
+        [TestCase(10, "X")]
+        [TestCase(50, "L")]
+        [TestCase(100, "C")]
+        [TestCase(500, "D")]
+        [TestCase(1000, "M")]
+        public void UnaDigita(int nombre, string romain)
+        {
+            var converter = new ChiffresRomains();
+            string result = converter.ConvertToRoman(nombre);
+            Assert.AreEqual(romain, result);
+        }
+
+        [TestCase(6, "VI")]
+        [TestCase(11, "XI")]
+        [TestCase(16, "XVI")]
+        [TestCase(1561, "MDLXI")]
+        [TestCase(1666, "MDCLXVI")]
+        public void OneForEachLetter(int nombre, string romain)
+        {
+            var converter = new ChiffresRomains();
+            string result = converter.ConvertToRoman(nombre);
+            Assert.AreEqual(romain, result);
+        }
+
+        [TestCase(2, "II")]
+        [TestCase(7, "VII")]
+        [TestCase(12, "XII")]
+        [TestCase(26, "XXVI")]
+        [TestCase(2000, "MM")]
+        [TestCase(2666, "MMDCLXVI")]
+        public void TwoOrThreeConsecutiveLetters(int nombre, string romain)
+        {
+            var converter = new ChiffresRomains();
+            string result = converter.ConvertToRoman(nombre);
+            Assert.AreEqual(romain, result);
+        }
+
+        [TestCase(4, "IV")]
+        [TestCase(42, "XLII")]
+        [TestCase(400, "CD")]
+        public void FourConsecutiveLetters(int nombre, string romain)
+        {
+            var converter = new ChiffresRomains();
+            string result = converter.ConvertToRoman(nombre);
+            Assert.AreEqual(romain, result);
+        }
+
+        [TestCase(900, "CM")]
+        [TestCase(1999, "MCMXCIX")]
+        public void FourConsecutiveLettersWithJump(int nombre, string romain)
+        {
+            var converter = new ChiffresRomains();
+            string result = converter.ConvertToRoman(nombre);
+            Assert.AreEqual(romain, result);
+        }
+
+        [TestCase(2888 ,"MMDCCCLXXXVIII")]
+        [TestCase(2444, "MMCDXLIV")]
+        [TestCase(1977, "MCMLXXVII")]
+        [TestCase(496, "CDXCVI")]
+        public void Complex(int number, string romanNumber)
+        {
+            var converter = new ChiffresRomains();
+            string result = converter.ConvertToRoman(number);
+            Assert.AreEqual(romanNumber, result);
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(3001)]
+        public void WrongNumber(int nombre)
+        {
+            var converter = new ChiffresRomains();
+            Assert.Throws<InvalidOperationException>(() => converter.ConvertToRoman(nombre));
+        }
+
+        [Test]
+        public void CovertToDigits()
+        {
+            var converter = new ChiffresRomains();
+            for (var i = 1; i < 3000; i++)
+            {
+                Assert.AreEqual(i, converter.ConvertToDigits(converter.ConvertToRoman(i)));
+            }
+        }
+    }
+}
